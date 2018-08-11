95becee07e51ee11568dbe3484435ea90f5c595c
Zhetchko Vitchev <notargetbg@gmail.com>
2018-08-11 11:12:16 +0300

    Initialization
    - init configs
    - base react app setup
    - libs and dependencies
    - simple build and hot reload setup

---
+++ src/components/Main.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import '../assets/styles/main.scss';
+
+export default class App extends React.Component {    
+
+    render() {
+        const boom = true;
+
+        return (
+            <div>
+                Here we go
+            </div>
+        );
+    }
+}
