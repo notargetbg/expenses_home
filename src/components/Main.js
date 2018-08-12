f92e1be92b92806dd870ffe444cb43e92dc4012b
Dilyana Mihaylova <dilyana.p.mihaylova@gmail.com>
2018-08-12 13:52:23 +0300

    Create general layout

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import '../assets/styles/main.scss';
+import Navigation from './layout/navbar.jsx';
+import Footer from './layout/footer.jsx';
 
 export default class App extends React.Component {    
 
     render() {
-        const boom = true;
 
         return (
             <div>
-                Here we go
+                <Navigation></Navigation>
+                <Footer></Footer>
             </div>
         );
     }
