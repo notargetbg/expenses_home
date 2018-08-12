07421feb2668ea5eb19509eac7771a92f8f094b5
Zhetchko Vitchev <notargetbg@gmail.com>
2018-08-12 19:00:04 +0300

    StatementType, SmartTable Components edits

--- src/components/layout/View.jsx
+++ src/components/layout/View.jsx
@@ -2,15 +2,16 @@ import React from 'react';
 import { Route, Link } from 'react-router-dom';
 import Home from '../views/Home.jsx';
 import About from '../views/About.jsx';
-import Type from '../views/Type.jsx';
+import StatementType from '../views/StatementType.jsx';
 
 export default class View extends React.Component {
 	render() {
+
 		return (
 			<div id='ui-view'>				
 				<Route exact path='/' component={Home}/>
 				<Route path='/about' component={About}/>
-				<Route path='/:type' component={Type}/>
+				<Route path='/statement/:type' component={StatementType}/>
 			</div>
 		);
 	}
