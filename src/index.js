807c6638ed1e615022c9b5de280f25ce4c9140b4
Zhetchko Vitchev <notargetbg@users.noreply.github.com>
2019-01-31 19:48:44 +0200

    basic redux setup

--- src/index.js
+++ src/index.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/Main';
+import { Provider } from 'react-redux';
+import { applyMiddleware, createStore, compose } from 'redux';
+import rootReducer from './store/reducers';
+// import { composeWithDevTools } from 'redux-devtools-extension';
 import 'bootstrap/scss/bootstrap.scss';
 import './assets/styles/main.scss';
 
+const appStore = createStore(rootReducer, undefined,  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+
+
+
 ReactDOM.render(
-	<App />,
+	<Provider store={appStore}>
+		<App />
+	</Provider>,
 	document.getElementById('app')
 );
---
+++ src/store/reducers/test.js
@@ -0,0 +1,8 @@
+const DEFAULT_STATE = {
+	test: 'yeeey'
+};
+
+export default function(state = DEFAULT_STATE, action) {
+
+	return state;
+}
